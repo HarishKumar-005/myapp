3b05847fbbaffdc3ec39d0c29b32e6d9f762401a
HarishKumar-05 <harishkumar.sp5511@outlook.com>
2025-11-27 23:25:09 +0530

    first commit

---
+++ src/main/java/com/example/myapp/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot App is Running on Render!";
+    }
+}
